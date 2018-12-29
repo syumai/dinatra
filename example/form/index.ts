01cde7e8425cb446a0a6acda286696c8775221d6
Shuichiro Fukuoka <syumai@gmail.com>
2018-12-29 10:03:13 +0900

    Use ejs for template

--- example/form/index.ts
+++ example/form/index.ts
@@ -1,44 +1,14 @@
 import { app, get, post } from '../../dinatra.ts';
-import { cwd, open, Buffer } from 'deno';
+import { cwd, copy, open, stdout } from 'deno';
+import { render } from 'https://syumai.github.io/dpl/dpl.ts';
 
-const currentDir = cwd();
-const template = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Form example</title>
-  </head>
-  <body>
-    <%BODY%>
-  </body>
-</html>`;
-
-const filePath = (filename: string): string => `${currentDir}/${filename}.html`;
-
-const readFile = async (filename: string): Promise<string> => {
-  const file = await open(filePath(filename));
-  const buf = new Buffer();
-  await buf.readFrom(file);
-  return buf.toString();
-};
-
-const render = async (body: string): Promise<string> => {
-  return template.replace('<%BODY%>', body);
-};
+const templatePath = `${cwd()}/index.ejs`;
 
 app(
-  get('/', async () => {
-    const form = await readFile('form');
-    return await render(form);
-  }),
-  post('/name', async ({ params }) => {
-    const form = await readFile('form');
-    const body = `<h2>
-  Hi, ${params.name}!
-</h2>
-${form}`;
-    return await render(body);
-  })
+  get('/', async () => await render(templatePath, { message: '' })),
+  post(
+    '/name',
+    async ({ params: { name } }) =>
+      await render(templatePath, { message: `Hi, ${name}!` })
+  )
 );
