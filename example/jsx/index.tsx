540e2eca9cb1e6da99aa8390984c489a6279e8ff
syumai <syumai@gmail.com>
2019-10-09 03:02:31 +0900

    Add TSX example

---
+++ example/jsx/index.tsx
@@ -0,0 +1,16 @@
+/* @jsx h */
+import {
+  h,
+  renderHTML,
+} from 'https://denopkg.com/syumai/deno-libs/jsx/renderer.ts';
+import { app, get } from 'https://denopkg.com/syumai/dinatra/mod.ts';
+
+app(
+  get('/', () =>
+    renderHTML(
+      <html>
+        <body>Hello, world!</body>
+      </html>
+    )
+  )
+);
